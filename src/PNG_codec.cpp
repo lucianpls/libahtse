7f848e50537a04662d68cd8c0dafafa73d2e8f97
Lucian Plesea <lucianpls@gmail.com>
2020-10-12 17:45:16 -0700

    Use longjmp for PNG codec

--- src/PNG_codec.cpp
+++ src/PNG_codec.cpp
@@ -19,20 +19,25 @@ NS_AHTSE_START
 static void flush_png(png_structp) {};
 
 // Do nothing for warnings
-static void pngWH(png_struct *png, png_const_charp message) {};
+static void pngWH(png_structp pngp, png_const_charp message) {};
 
 // Error function
-static void pngEH(png_struct *png, png_const_charp message)
+static void pngEH(png_structp pngp, png_const_charp message)
 {
-    throw message;
+    codec_params* params = reinterpret_cast<codec_params*>(png_get_error_ptr(pngp));
+    strncpy(params->error_message, message, 1024);
+    longjmp(png_jmpbuf(pngp), 1);
 }
 
 // Read memory handler for PNG
 static void get_data(png_structp pngp, png_bytep data, png_size_t length)
 {
     storage_manager *src = static_cast<storage_manager *>(png_get_io_ptr(pngp));
-    if (static_cast<png_size_t>(src->size) < length)
-        throw "PNG decode expects more data than given";
+    if (static_cast<png_size_t>(src->size) < length) {
+        codec_params* params = (codec_params*)(png_get_error_ptr(pngp));
+        strcpy(params->error_message, "PNG decode expects more data than given");
+        longjmp(png_jmpbuf(pngp), 1);
+    }
     memcpy(data, src->buffer, length);
     src->buffer += length;
     src->size -= static_cast<int>(length);
@@ -42,8 +47,11 @@ static void get_data(png_structp pngp, png_bytep data, png_size_t length)
 static void store_data(png_structp pngp, png_bytep data, png_size_t length)
 {
     storage_manager *dst = static_cast<storage_manager *>(png_get_io_ptr(pngp));
-    if (static_cast<png_size_t>(dst->size) < length)
-        throw static_cast<png_const_charp>("PNG encode buffer overflow");
+    if (static_cast<png_size_t>(dst->size) < length) {
+        codec_params* params = (codec_params*)(png_get_error_ptr(pngp));
+        strcpy(params->error_message, "PNG encode buffer overflow");
+        longjmp(png_jmpbuf(pngp), 1);
+    }
     memcpy(dst->buffer, data, length);
     dst->buffer += length;
     dst->size -= static_cast<int>(length);
@@ -54,69 +62,69 @@ const char *png_stride_decode(codec_params &params,
     storage_manager &src,
     void *buffer)
 {
-    char *message = NULL;
-    png_structp pngp = NULL;
-    png_infop infop = NULL;
+    char *message = nullptr;
+    png_structp pngp = nullptr;
+    png_infop infop = nullptr;
     std::vector<png_bytep> png_rowp(static_cast<int>(raster.pagesize.y));
     for (size_t i = 0; i < png_rowp.size(); i++) // line_stride is always in bytes
         png_rowp[i] = reinterpret_cast<png_bytep>(
             static_cast<char *>(buffer)+i * params.line_stride);
 
-    try {
-        png_uint_32 width, height;
-        int bit_depth, ct;
-        pngp = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, pngEH, pngEH);
-        infop = png_create_info_struct(pngp);
-        png_set_read_fn(pngp, &src, get_data);
+    png_uint_32 width, height;
+    int bit_depth, ct;
+    pngp = png_create_read_struct(PNG_LIBPNG_VER_STRING, &params, pngEH, pngEH);
+    if (!pngp)
+        return "PNG error while creating decode PNG structure";
+    infop = png_create_info_struct(pngp);
+    if (!infop)
+        return "PNG error while creating decode info structure";
 
-        // This reads all chunks up to the first IDAT
-        png_read_info(pngp, infop);
-        png_get_IHDR(pngp, infop, &width, &height, &bit_depth, &ct, NULL, NULL, NULL);
+    if (setjmp(png_jmpbuf(pngp)))
+        return params.error_message;
 
-        if (static_cast<png_uint_32>(raster.pagesize.y) != height 
-            || static_cast<png_uint_32>(raster.pagesize.x) != width)
-            throw "Input PNG has the wrong size";
+    png_set_read_fn(pngp, &src, get_data);
 
-        if (png_get_rowbytes(pngp, infop) > params.line_stride)
-            throw "Wrong type of data in PNG decode";
+    // This reads all chunks up to the first IDAT
+    png_read_info(pngp, infop);
+
+    png_get_IHDR(pngp, infop, &width, &height, &bit_depth, &ct, NULL, NULL, NULL);
+
+    if (static_cast<png_uint_32>(raster.pagesize.y) != height
+        || static_cast<png_uint_32>(raster.pagesize.x) != width) {
+        strcpy(params.error_message, "Input PNG has the wrong size");
+        longjmp(png_jmpbuf(pngp), 1);
+    }
+
+    if ((raster.datatype == GDT_Byte && bit_depth != 8) ||
+        ((raster.datatype == GDT_UInt16 || raster.datatype == GDT_Short) && bit_depth != 16)) {
+        strcpy(params.error_message, "Input PNG has the wrong type");
+        longjmp(png_jmpbuf(pngp), 1);
+    }
 
 #if defined(NEED_SWAP)
-        if (bit_depth > 8)
-            png_set_swap(pngp);
+    if (bit_depth > 8)
+        png_set_swap(pngp);
 #endif
 
-        // TODO: Decode to expected format
-        //        png_set_palette_to_rgb(pngp); // Palette to RGB
-        //        png_set_tRNS_to_alpha(pngp);  // transparency palette to Alpha
-        //        png_set_add_alpha(pngp, 255, PNG_FILLER_AFTER); // Add alpha if not there
-        //        png_read_update_info(pngp, infop); // update the reader
+    // TODO: Decode to expected format
+    // png_set_palette_to_rgb(pngp); // Palette to RGB
+    // png_set_tRNS_to_alpha(pngp); // transparency to Alpha
+    // png_set_add_alpha(pngp, 255, PNG_FILTER_AFTER); // Add alpha if not there
+    // Call this after using any of the png_set_*
+    png_read_update_info(pngp, infop);
 
-        // Call this after using any of the png_set_*
-        png_read_update_info(pngp, infop);
-
-        png_read_image(pngp, png_rowp.data());
-        png_read_end(pngp, infop);
-    }
-    catch (const char *error) {
-        message = params.error_message;
-        strcpy(message, error);
-    }
-    catch (...) { // Just in case
-        message = params.error_message;
-        strcpy(message, "Unknown PNG decode error");
-    }
-
+    png_read_image(pngp, png_rowp.data());
+    png_read_end(pngp, infop);
     png_destroy_read_struct(&pngp, &infop, 0);
 
-    return message;
+    return nullptr;
 }
 
 const char *png_encode(png_params &params, const TiledRaster &raster, 
     storage_manager &src, storage_manager &dst)
 {
-    char *message = NULL;
-    png_structp pngp = NULL;
-    png_infop infop = NULL;
+    png_structp pngp = nullptr;
+    png_infop infop = nullptr;
     png_uint_32 width = static_cast<png_uint_32>(raster.pagesize.x);
     png_uint_32 height = static_cast<png_uint_32>(raster.pagesize.y);
     // Use a vector so it cleans up itself
@@ -125,60 +133,54 @@ const char *png_encode(png_params &params, const TiledRaster &raster,
     // To avoid changing the buffer pointer
     storage_manager mgr = dst;
 
-    try {
-        pngp = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, pngEH, pngWH);
-        infop = png_create_info_struct(pngp);
-        png_set_write_fn(pngp, &mgr, store_data, flush_png);
+    pngp = png_create_write_struct(PNG_LIBPNG_VER_STRING, &params, pngEH, pngWH);
+    if (!pngp)
+        return "PNG error while creating encoding PNG structure";
+    infop = png_create_info_struct(pngp);
+    if (!infop)
+        return "PNG error while creating encoding info structure";
+    if (setjmp(png_jmpbuf(pngp)))
+        return params.error_message;
 
-        png_set_IHDR(pngp, infop, width, height, params.bit_depth, params.color_type,
-            PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_set_write_fn(pngp, &mgr, store_data, flush_png);
+    png_set_IHDR(pngp, infop, width, height, params.bit_depth, params.color_type,
+        PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_set_compression_level(pngp, params.compression_level);
 
-        png_set_compression_level(pngp, params.compression_level);
+    // Flag NDV as transparent color
+    if (params.has_transparency) {
+        // TODO: Pass the transparent color via params.
+        // For now, 0 is the no data value, regardless of the type of data
 
-        // Flag NDV as transparent color
-        if (params.has_transparency) {
-            // TODO: Pass the transparent color via params.
-            // For now, 0 is the no data value, regardless of the type of data
-
-            png_color_16 tcolor;
-            memset(&tcolor, 0, sizeof(png_color_16));
-            png_set_tRNS(pngp, infop, 0, 0, &tcolor);
-        }
-
-        png_write_info(pngp, infop);
+        png_color_16 tcolor;
+        memset(&tcolor, 0, sizeof(png_color_16));
+        png_set_tRNS(pngp, infop, 0, 0, &tcolor);
+    }
 
 #if defined(NEED_SWAP)
-        if (params.bit_depth > 8)
-            png_set_swap(pngp);
+    if (params.bit_depth > 8)
+        png_set_swap(pngp);
 #endif
 
-        auto rowbytes = png_get_rowbytes(pngp, infop);
-        for (size_t i = 0; i < png_rowp.size(); i++)
-            png_rowp[i] = reinterpret_cast<png_bytep>(src.buffer + i * rowbytes);
+    auto rowbytes = png_get_rowbytes(pngp, infop);
+    for (size_t i = 0; i < png_rowp.size(); i++)
+        png_rowp[i] = reinterpret_cast<png_bytep>(src.buffer + i * rowbytes);
 
-        png_write_image(pngp, png_rowp.data());
-        png_write_end(pngp, infop);
-    }
-    catch (const char *error) {
-        message = params.error_message;
-        strcpy(message, error);
-    }
-    catch (...) { // Just in case
-        message = params.error_message;
-        strcpy(message, "Unknown PNG encode error");
-    }
+    png_write_info(pngp, infop);
+    png_write_image(pngp, png_rowp.data());
+    png_write_end(pngp, infop);
 
     png_destroy_write_struct(&pngp, &infop);
     dst.size -= mgr.size; // mgr.size is bytes left
 
-    return message;
+    return nullptr;
 }
 
 int set_def_png_params(const TiledRaster &raster, png_params *params) {
     // Pick some defaults
     // Only handles 8 or 16 bits
     memset(params, 0, sizeof(png_params));
-    params->bit_depth = raster.datatype == GDT_Byte ? 8 : 16;
+    params->bit_depth = (raster.datatype == GDT_Byte) ? 8 : 16;
     params->compression_level = 6;
     params->has_transparency = FALSE;
 
