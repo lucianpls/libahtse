65e15a938245b20d0c72ed39c8240fafbdbed4ee
Lucian Plesea <lucianpls@gmail.com>
2020-12-09 15:55:16 -0800

    Always set projection, default to self 0-1

--- src/ahtse_util.cpp
+++ src/ahtse_util.cpp
@@ -251,8 +251,8 @@ const char *configRaster(apr_pool_t *pool, apr_table_t *kvp, TiledRaster &raster
     if (nullptr != (line = apr_table_get(kvp, "SkippedLevels")))
         raster.skip = int(apr_atoi64(line));
 
-    if (nullptr != (line = apr_table_get(kvp, "Projection")))
-        raster.projection = line ? apr_pstrdup(pool, line) : "WM";
+    line = apr_table_get(kvp, "Projection");
+    raster.projection = line ? apr_pstrdup(pool, line) : "SELF";
 
     if (nullptr != (line = apr_table_get(kvp, "NoDataValue")))
         raster.ndv = get_value(line, &raster.has_ndv);
